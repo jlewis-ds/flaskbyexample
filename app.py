b277b871373956156f3c1c1c7116b8e4bd3a17fd
J Lewis <justin.jay.lewis@gmail.com>
2020-01-21 15:29:09 -0800

    updated environment variables

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import os
 from flask import Flask
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
@@ -10,4 +12,4 @@ def hello_name(name):
     return "Hello {}!".format(name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
