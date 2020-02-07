d12514cc3c2fc05d95970c7072e580044379cf26
J Lewis <justin.jay.lewis@gmail.com>
2020-02-06 18:05:40 -0800

    working dbs

--- fbe/Scripts/alembic-script.py
+++ fbe/Scripts/alembic-script.py
@@ -1,6 +1,6 @@
 #!c:\users\jlewis\documents\programming\flaskbyexample\fbe\scripts\python.exe
-# EASY-INSTALL-ENTRY-SCRIPT: 'alembic==1.3.2','console_scripts','alembic'
-__requires__ = 'alembic==1.3.2'
+# EASY-INSTALL-ENTRY-SCRIPT: 'alembic==1.3.3','console_scripts','alembic'
+__requires__ = 'alembic==1.3.3'
 import re
 import sys
 from pkg_resources import load_entry_point
@@ -8,5 +8,5 @@ from pkg_resources import load_entry_point
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
     sys.exit(
-        load_entry_point('alembic==1.3.2', 'console_scripts', 'alembic')()
+        load_entry_point('alembic==1.3.3', 'console_scripts', 'alembic')()
     )
